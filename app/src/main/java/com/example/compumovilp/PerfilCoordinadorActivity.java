07a77c2b371e46375fa19d31414fb0c88d9f6529
Kevin López <kevinleonardolopez@gmail.com>
2023-04-18 12:19:01 -0500

    Solicitudes Finca Coordinador

--- app/src/main/java/com/example/compumovilp/PerfilCoordinadorActivity.java
+++ app/src/main/java/com/example/compumovilp/PerfilCoordinadorActivity.java
@@ -2,13 +2,32 @@ package com.example.compumovilp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.compumovilp.databinding.ActivityLoginBinding;
+import com.example.compumovilp.databinding.ActivityPerfilCoordinadorBinding;
 
 public class PerfilCoordinadorActivity extends AppCompatActivity {
+
+    private ActivityPerfilCoordinadorBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil_coordinador);
+        binding = ActivityPerfilCoordinadorBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        binding.solicitudFincaUsuarioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), SolicitudFincaCoordinador.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/compumovilp/SolicitudFincaCoordinador.java
@@ -0,0 +1,39 @@
+package com.example.compumovilp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.compumovilp.databinding.ActivityMainBinding;
+import com.example.compumovilp.databinding.ActivitySolicitudFincaCoordinadorBinding;
+
+public class SolicitudFincaCoordinador extends AppCompatActivity {
+
+    private ActivitySolicitudFincaCoordinadorBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySolicitudFincaCoordinadorBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.verSolicitudBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.rutaSedebton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
